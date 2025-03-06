2513767a7d3131f962c03ee40230c9ac2a98ac45
jozu-lacerda <josuejuniorlacerda@gmail.com>
2025-03-05 21:17:23 -0300

    primiero commit

---
+++ roteiro_romantico.js
@@ -0,0 +1,4 @@
+// Script para exibir o roteiro romântico na página 2
+function mostrarRoteiro() {
+    alert("Aqui está um roteiro romântico para você:\n1. Comer no TACO BELL\n2. PASSEAR NO MUSEU\n3. JOGAR VALORANT");
+}
